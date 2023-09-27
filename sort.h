8058694728064fa1054d8a727027d868717e663c
Adabs-hub <emmanueladabogo@gmail.com>
2023-09-26 18:16:27 -0700

    define header in sort.h

--- sort.h
+++ sort.h
@@ -22,4 +22,5 @@ typedef struct listint_s
 	void print_array(const int *array, size_t size);
 	void bubble_sort(int *array, size_t size);
 	void insertion_sort_list(listint_t **list);
+	void selection_sort(int *array, size_t size);
 #endif /*_SORT_H*/
